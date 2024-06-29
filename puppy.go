0c5a62384936fb2c8144da121bb554cf56bef9db
Lance Watanabe <phiferthree@gmail.com>
2024-06-29 12:46:35 -0700

    v2

--- puppy.go
+++ puppy.go
@@ -1,9 +1,15 @@
 package puppy
 
+import "fmt"
+
 func Bark() string {
 	return "woof!"
 }
 
 func Barks() string {
 	return "woof woof woof" // test
+}
+
+func Version2() {
+	fmt.Println("from version 2")
 }
