91078438dd0fd366cf8066f08f53a60e47358cbd
Lance Watanabe <phiferthree@gmail.com>
2024-07-07 10:29:52 -0700

    better comments

--- puppy.go
+++ puppy.go
@@ -3,19 +3,22 @@ package puppy
 
 import "fmt"
 
-// Bark says woof
+// Bark returns the sound "woof"
 func Bark() string {
 	return "woof!"
 }
 
+// Barks returns the sound "woof woof woof"
 func Barks() string {
 	return "woof woof woof" // test
 }
 
+// Tag2 prints a message indicating it's from tag 1.0.1
 func Tag2() {
 	fmt.Println("from tag 1.0.1")
 }
 
+// Tag3 prints a message indicating it's from tag 1.0.2
 func Tag3() {
 	fmt.Println("from tag 1.0.2")
 }
